1d4091b597b85fb5e394c5d90277716ab10d34eb
Sky Seulki <admin@skys-mbp.home>
2019-03-25 21:49:00 -0700

    completed 1 data types

---
+++ js/app.js
@@ -0,0 +1,28 @@
+console.log("Week 2 - Monday HW");
+
+// 1 Data types
+// a light switch that can be either on or off
+/// boolean
+// => const lightSwitch = ();
+// if (lightSwitch === on) {
+// => console.log("Switch on!") }
+
+// a user's email address
+/// array
+/// const user = ("user@me.com")
+
+// a spaceship with a hull, laser blasters, tracotr beams, and warp drive
+/// objects 
+/// const spaceship ={
+//     hull: 2
+//     laser blasters; 1
+// }
+
+// a list of student names from our class
+/// objects of arrays
+
+// a list of student names from our class, each with a location
+/// objects with arrays
+
+// list of student names from our class, each with a location and each with a list of favorite tv shows
+/// objects with arrays and strings for tv shows
