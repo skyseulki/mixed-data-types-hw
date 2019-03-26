7349b30cde918fb83f409602667d66c0359f0908
Sky Seulki <admin@skys-mbp.home>
2019-03-25 22:11:23 -0700

    completed part 3 crazy objects

--- js/app.js
+++ js/app.js
@@ -43,3 +43,49 @@ const sky = {
 }
 /// write a code that will access your hobby from the object that you just created
 sky.hobby;
+
+
+// Crazy object
+const crazyObject = {
+    taco: [
+      {
+        meat: 'steak',
+        cheese: ['panela', 'queso', 'chihuahua']
+      },
+      {
+        meat: 'chicken',
+        salsa: ["pico", "hot", "hotter", "really hot", "really really hot", "omg my mouth is burning"]
+      },
+    ],
+    larry: {
+      nicknames: ["LD", "Chicken Teriyaki Boyyyyyy"],
+      quotes: ["Pretty pretty prettayyyyy good", "Is that a parkinson's thing?", "women love a self confident bald man", "I'm a walking talking enigma"],
+      characters: [
+        {
+          name: "Jeff",
+          occupation: "manager"
+        },
+        {
+          name: "funkhauser",
+          occupation: "tv dude"
+        },
+        {
+          name: "susie",
+          occupation: "jeffs wife",
+          favourtieHobby: "Swearing at Larry and Jeff"
+        },
+      ]
+    }
+}
+
+/// use crazyObject to log the following
+// "omg my mouth is burning"
+crazyObject.taco[1].salsa[5];
+// "Pretty pretty prettayyyyy good"
+crazyObject.larry["quotes"][0];
+// "Swearing at Larry and Jeff"
+crazyObject.larry.characters[2].favourtieHobby;
+// "Chicken Teriyaki Boyyyyyy"
+crazyObject.larry["nicknames"][1];
+// The object the contains the name funkhauser
+crazyObject.larry.characters[1].name;
