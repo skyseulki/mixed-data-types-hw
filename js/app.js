2af590e72f69543aa695ea2832e6236cdace1feb
Sky Seulki <admin@skys-mbp.home>
2019-03-25 22:00:30 -0700

    completed part 2 data structures

--- js/app.js
+++ js/app.js
@@ -26,3 +26,20 @@ console.log("Week 2 - Monday HW");
 
 // list of student names from our class, each with a location and each with a list of favorite tv shows
 /// objects with arrays and strings for tv shows
+
+
+// 2 Take it easy
+const rainbow = ["red", "orange", "yellow", "green", "blue", "purple"];
+
+/// write code that will access "blue" from the rainbow array
+rainbow[4];
+
+/// write obj that is called your name and holds the information about your favorite food, a hobby, the name of the town that you live in currently, and your favorite datatype
+const sky = {
+    favoriteFood: "tacos",
+    hobby: "yoga",
+    location: "Alhambra",
+    favoriteDatatype: "object"
+}
+/// write a code that will access your hobby from the object that you just created
+sky.hobby;
